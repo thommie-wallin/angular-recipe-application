28e0da2b7fc2d293d04199f3625641c99261524a
thommie-wallin <thommie.wallin@chasacademy.se>
2024-06-13 19:30:09 +0200

    add angular-material-progress-spinner in the loading-component and add style, adjust style in the recipe-detail for instruction-title-h3

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { GlobalStateService } from '../../../state';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-loading',
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinnerModule],
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.css'
 })
