890ae60902692de0fbfe355d80e9ae6e149505e5
thommie-wallin <thommie.wallin@chasacademy.se>
2024-06-28 15:28:02 +0200

    implement dynamic routes to card component to use the component with user-recipe-list, style recipe-user-detail compoment

--- src/app/features/user-recipes/components/user-recipe-detail/user-recipe-detail.component.ts
+++ src/app/features/user-recipes/components/user-recipe-detail/user-recipe-detail.component.ts
@@ -7,20 +7,24 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { FavouriteButtonComponent, LoadingComponent, SafeHtmlPipe } from '../../../../shared';
+import { ErrorComponent, FavouriteButtonComponent, LoadingComponent, SafeHtmlPipe } from '../../../../shared';
+import { GlobalStateService } from '../../../../state';
 
 @Component({
   selector: 'app-user-recipe-detail',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatMenuModule, MatIconModule, MatDividerModule, FavouriteButtonComponent, LoadingComponent, SafeHtmlPipe],
+  imports: [CommonModule, MatCardModule, MatMenuModule, MatIconModule, MatDividerModule, FavouriteButtonComponent, LoadingComponent, SafeHtmlPipe, LoadingComponent, ErrorComponent],
   templateUrl: './user-recipe-detail.component.html',
   styleUrl: './user-recipe-detail.component.css'
 })
 export class UserRecipeDetailComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private userRecipesStateService = inject(UserRecipesStateService);
+  private globalStateService = inject(GlobalStateService);
 
   recipe: UserRecipe | undefined;
+  loading = this.globalStateService.loading;
+  error = this.globalStateService.error;
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
