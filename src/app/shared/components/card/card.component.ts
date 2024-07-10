9356128b2e63e378b1716d9e4a21285cdc55a0fa
thommie-wallin <thommie.wallin@chasacademy.se>
2024-07-10 18:48:32 +0200

    create buttons for edit and delete in card-component when in user-recipe-list

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -3,11 +3,13 @@ import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { Recipe } from '../../../features/browse';
 import { FavouriteButtonComponent } from '../favourite-button/favourite-button.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, RouterModule, FavouriteButtonComponent],
+  imports: [MatCardModule, RouterModule, FavouriteButtonComponent, MatIconModule, MatButtonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
